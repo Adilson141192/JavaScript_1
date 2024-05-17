3980ce452b8a660208a54c55a690fb378676597f
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-05-17 13:15:13 -0300

    051724 aula0304

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
