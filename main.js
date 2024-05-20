11b9485d850f77865aece6ce63e8877257cac573
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-05-20 10:29:57 -0300

    052024 aula0409

--- main.js
+++ main.js
@@ -4,24 +4,15 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
 
-//enquanto
-while (contador < listaDeTeclas.length) {
-
-    const tecla = listaDeTeclas[contador]
+    const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    //template string
-    const idAudio = `#som_${instrumento}`
-
-    //console.log(idAudio)
+    const idAudio = `#som_${instrumento}`;//template string
 
     tecla.onclick = function () {
-        tocaSom(idAudio)
+        tocaSom(idAudio);
     }
-
-    contador = contador + 1;
-
-    //console.log(contador);
 
 }
