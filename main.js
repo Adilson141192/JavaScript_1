161b6fade83bf816ce79b578381dc574d2c2e0dc
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-05-20 09:40:20 -0300

    052024 aula0404

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -9,7 +8,8 @@ let contador = 0
 
 //enquanto
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    listaDeTeclas[contador].onclick = tocaSom;
 
     contador = contador + 1;
 
